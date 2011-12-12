84c9f8993091a98f9b5e4fba05fd78904105ee28
Vasily Alexeev <mail@v-alexeev.ru>
2011-12-12 17:38:38 +0400

    Fixed edit_user_details; PEP-8 function naming (used stupid names from official Yandex API before)

--- YandexMailApi.py
+++ YandexMailApi.py
@@ -51,8 +51,8 @@ class YandexMailApi(object):
     def __init__(self, api_token):
         self.token = api_token
 
-    def runCommand(self, command_name, params, response_handler=None):
-        if response_handler is None: response_handler = self.simpleResponseHandler
+    def run_command(self, command_name, params, response_handler=None):
+        if response_handler is None: response_handler = self.simple_response_handler
         params.pop('self', None)
         params = dict((k,v) for k,v in params.iteritems() if v)
         params['token'] = self.token
@@ -78,13 +78,13 @@ class YandexMailApi(object):
 
 # Response handlers
     @staticmethod
-    def simpleResponseHandler(xml): # Simple ok/error response
+    def simple_response_handler(xml): # Simple ok/error response
         if xml.find('ok') is not None:
             return True
 
 
     @staticmethod
-    def okAttrResponseHandler(xml): # Response with meaningful <ok/> attributes
+    def ok_attr_response_handler(xml): # Response with meaningful <ok/> attributes
         ok_tag = xml.find('ok')
         if ok_tag is not None:
             result = {}
@@ -107,7 +107,7 @@ class YandexMailApi(object):
 
 
     @staticmethod
-    def checkUserHandler(xml): # check_user is a special case
+    def check_user_handler(xml): # check_user is a special case
         tag = xml.find('result')
         if tag.text == "exists":
             return True
@@ -116,7 +116,7 @@ class YandexMailApi(object):
         
 
     @staticmethod
-    def responseHandlerFactory(response_template):
+    def response_handler_factory(response_template):
         """
             response_template should be a dictionary describing structure of the response.
 
@@ -140,7 +140,7 @@ class YandexMailApi(object):
 
             (the same goes for command signatures, they are pretty useless except as documentation)
         """
-        def responseHandler(xml):
+        def response_handler(xml):
             def parseXml(xml, template):
                 result = {}
                 for tag_name in template:
@@ -165,13 +165,13 @@ class YandexMailApi(object):
             
             return parseXml(xml, response_template)
 
-        return responseHandler
+        return response_handler
 
 
-    @classmethod # Had to make it class method to be able to reference responseHandlerFactory
-    def authResponseHandlerWrapper(cls, error_return_path):
-        def authResponseHandler(xml):
-            first_pass = cls.responseHandlerFactory(
+    @classmethod # Had to make it class method to be able to reference response_handler_factory
+    def auth_response_handler_wrapper(cls, error_return_path):
+        def auth_response_handler(xml):
+            first_pass = cls.response_handler_factory(
                     {
                          'name': unicode,
                          'email':
@@ -186,7 +186,7 @@ class YandexMailApi(object):
                 result = ''.join((result, '&error_retpath=', urllib.quote_plus(error_return_path)))
             return result
 
-        return authResponseHandler
+        return auth_response_handler
     
 
 ###################
@@ -195,38 +195,38 @@ class YandexMailApi(object):
 
 # Operations with users
         
-    def createUser(self, u_login, u_password):
-        return self.runCommand('reg_user_token', locals())
+    def create_user(self, u_login, u_password):
+        return self.run_command('reg_user_token', locals())
 
-    def checkUserExistence(self, login):
-        return self.runCommand('check_user', locals(), response_handler=self.checkUserHandler)
+    def check_user_existence(self, login):
+        return self.run_command('check_user', locals(), response_handler=self.check_user_handler)
 
-    def deleteUser(self, login):
-        return self.runCommand('delete_user', locals())
+    def delete_user(self, login):
+        return self.run_command('delete_user', locals())
 
-    def editUserDetails(self, login, new_password = None, iname = None, fname = None, sex = None):
+    def edit_user_details(self, login, password = None, iname = None, fname = None, sex = None):
         """
         Sex: 1 - male; 2 - female
         iname - first name
         fname - last name
 
         """
-        return self.runCommand('edit_user', locals())
+        return self.run_command('edit_user', locals())
 
-    def setDefaultDomainUser(self, domain, login):
-        return self.runCommand('api/reg_default_user', locals(),
-            response_handler=self.responseHandlerFactory(
+    def set_default_domain_user(self, domain, login):
+        return self.run_command('api/reg_default_user', locals(),
+            response_handler=self.response_handler_factory(
                     {
                          'name': unicode,
                          'default-email': unicode,
                     }))
 
-    def getUnreadMessagesCount(self, login):
-        return self.runCommand('get_mail_info', locals(), response_handler=self.okAttrResponseHandler)
+    def get_unread_messages_count(self, login):
+        return self.run_command('get_mail_info', locals(), response_handler=self.ok_attr_response_handler)
 
-    def getUserInfo(self, login):
-        return self.runCommand('get_user_info', locals(),
-            response_handler=self.responseHandlerFactory(
+    def get_user_info(self, login):
+        return self.run_command('get_user_info', locals(),
+            response_handler=self.response_handler_factory(
                     {
                          'name': unicode,
                          'user': {
@@ -245,9 +245,9 @@ class YandexMailApi(object):
                          },
                     }))
 
-    def getUsersList(self, page = 1, perpage = 100):
-        return self.runCommand('get_domain_users', locals(),
-            response_handler=self.responseHandlerFactory(
+    def get_users_list(self, page = 1, perpage = 100):
+        return self.run_command('get_domain_users', locals(),
+            response_handler=self.response_handler_factory(
                     {'emails':
                         {'action-status': unicode,
                         'email': [{'name': unicode}],
@@ -259,17 +259,17 @@ class YandexMailApi(object):
                     'emails-max-count': unicode,
                     }))
 
-    def createDomainUser(self, domain, login, passwd=None):
-        return self.runCommand('api/reg_user', locals(),
-            response_handler=self.responseHandlerFactory(
+    def create_domain_user(self, domain, login, passwd=None):
+        return self.run_command('api/reg_user', locals(),
+            response_handler=self.response_handler_factory(
                     {
                          'name': unicode,
                          'email': {'name': unicode},
                     }))
 
-    def delDomainUser(self, domain, login):
-        return self.runCommand('api/del_user', locals(),
-            response_handler=self.responseHandlerFactory(
+    def del_domain_user(self, domain, login):
+        return self.run_command('api/del_user', locals(),
+            response_handler=self.response_handler_factory(
                     {
                          'name': unicode,
                          'email': {'name': unicode},
@@ -277,95 +277,95 @@ class YandexMailApi(object):
 
 # Import from old mailboxes
 
-    def setImportSettings(self, method, ext_serv, ext_port = None, isssl = "no", callback = None):
-        return self.runCommand('set_domain', locals())
+    def set_import_settings(self, method, ext_serv, ext_port = None, isssl = "no", callback = None):
+        return self.run_command('set_domain', locals())
 
-    def startImport(self, login, ext_login, password):
-        return self.runCommand('start_import', locals())
+    def start_import(self, login, ext_login, password):
+        return self.run_command('start_import', locals())
 
-    def getImportState(self, login):
-        return self.runCommand('check_import', locals(), response_handler=self.okAttrResponseHandler)
+    def get_import_state(self, login):
+        return self.run_command('check_import', locals(), response_handler=self.ok_attr_response_handler)
 
-    def registerAndStartImport(self, login, inn_password, ext_login, ext_password):
-        return self.runCommand('reg_and_imp', locals())
+    def register_and_start_import(self, login, inn_password, ext_login, ext_password):
+        return self.run_command('reg_and_imp', locals())
 
-    def stopImport(self, login):
-        return self.runCommand('stop_import', locals())
+    def stop_import(self, login):
+        return self.run_command('stop_import', locals())
 
 # Authentication
 
-    def getAuthUrl(self, domain, login, error_return_path=None):
-        return self.runCommand('api/user_oauth_token', locals(),
-                               response_handler=self.authResponseHandlerWrapper(error_return_path))
+    def get_auth_url(self, domain, login, error_return_path=None):
+        return self.run_command('api/user_oauth_token', locals(),
+                               response_handler=self.auth_response_handler_wrapper(error_return_path))
 
-    def setUrlCallback(self, domain_name, callback, error_return_path=None):
+    def set_url_callback(self, domain_name, callback, error_return_path=None):
         """
             Callback should be URL-encoded
         """
-        return self.runCommand('api/set_mail_callback', locals(),
-                               response_handler=self.authResponseHandlerWrapper(error_return_path))
+        return self.run_command('api/set_mail_callback', locals(),
+                               response_handler=self.auth_response_handler_wrapper(error_return_path))
 
 # Filters
 
-    def getFilters(self, login):
-        return self.runCommand('get_forward_list', locals(),
-            response_handler=self.responseHandlerFactory({'filters': [{'filter': unicode}]}))
+    def get_filters(self, login):
+        return self.run_command('get_forward_list', locals(),
+            response_handler=self.response_handler_factory({'filters': [{'filter': unicode}]}))
 
-    def delFilter(self, login, filter_id):
-        return self.runCommand('delete_forward', locals())
+    def del_filter(self, login, filter_id):
+        return self.run_command('delete_forward', locals())
 
 # Operations with domains
 
-    def addDomain(self, domain):
-        return self.runCommand('api/reg_domain', locals(),
-            response_handler=self.responseHandlerFactory(
+    def add_domain(self, domain):
+        return self.run_command('api/reg_domain', locals(),
+            response_handler=self.response_handler_factory(
             {
                         'name': unicode,
                         'secret_name': unicode,
                         'secret_value': unicode,
             }))
 
-    def delDomain(self, domain):
-        return self.runCommand('api/del_domain', locals(),
-            response_handler=self.responseHandlerFactory(
+    def del_domain(self, domain):
+        return self.run_command('api/del_domain', locals(),
+            response_handler=self.response_handler_factory(
             {
                          'name': unicode,
             }))
 
-    def addLogo(self, domain):
-        return self.runCommand('api/add_logo', locals(),
-            response_handler=self.responseHandlerFactory(
+    def add_logo(self, domain):
+        return self.run_command('api/add_logo', locals(),
+            response_handler=self.response_handler_factory(
                 {'domain': {'name': unicode,},
                  'logo': {'url': unicode,}
                 }
             ))
 
-    def delLogo(self, domain):
-        return self.runCommand('api/del_logo', locals(),
-            response_handler=self.responseHandlerFactory(
+    def del_logo(self, domain):
+        return self.run_command('api/del_logo', locals(),
+            response_handler=self.response_handler_factory(
             {'name': unicode,}))
 
 # Operations with administrators
 
-    def addAdmin(self, domain, login):
-        return self.runCommand('api/multiadmin/add_admin', locals(),
-            response_handler=self.responseHandlerFactory(
+    def add_admin(self, domain, login):
+        return self.run_command('api/multiadmin/add_admin', locals(),
+            response_handler=self.response_handler_factory(
             {
                 'name': unicode,
                 'new-admin': unicode,
             }))
 
-    def delAdmin(self, domain, login):
-        return self.runCommand('api/multiadmin/del_admin', locals(),
-            response_handler=self.responseHandlerFactory(
+    def del_admin(self, domain, login):
+        return self.run_command('api/multiadmin/del_admin', locals(),
+            response_handler=self.response_handler_factory(
             {
                 'name': unicode,
                 'new-admin': unicode,
             }))
 
-    def listAdmins(self, domain):
-        return self.runCommand('api/multiadmin/get_admins', locals(),
-            response_handler=self.responseHandlerFactory(
+    def list_admins(self, domain):
+        return self.run_command('api/multiadmin/get_admins', locals(),
+            response_handler=self.response_handler_factory(
             {
                 'name': unicode,
                 'other-admins': [{'login': unicode,}],
@@ -373,16 +373,16 @@ class YandexMailApi(object):
 
 # Operations with mailing lists
 
-    def createGeneralMaillist(self, domain, ml_name):
-        return self.runCommand('api/create_general_maillist', locals(),
-            response_handler=self.responseHandlerFactory(
+    def create_general_maillist(self, domain, ml_name):
+        return self.run_command('api/create_general_maillist', locals(),
+            response_handler=self.response_handler_factory(
             {
                 'name': unicode,
             }))
 
-    def deleteGeneralMaillist(self, domain):
-        return self.runCommand('api/delete_general_maillist', locals(),
-            response_handler=self.responseHandlerFactory(
+    def delete_general_maillist(self, domain):
+        return self.run_command('api/delete_general_maillist', locals(),
+            response_handler=self.response_handler_factory(
             {
                 'name': unicode,
             }))
